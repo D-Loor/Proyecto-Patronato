6d243f577980e9e681f0226cf548d7dfeb39b251
Diego Oswaldo Loor Moran <diego18.loor@gmail.com>
2021-04-03 13:51:00 -0500

    Validacion

--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -50,3 +50,97 @@ $(document).ready(function() {
 		});
 	};
 });
+
+
+//Validar cédula
+
+function validarC() {
+	var valido=document.getElementById("vcedula");
+	debugger
+	var numero = document.getElementById("cedula").value;
+	
+
+      var suma = 0;      
+      var residuo = 0;      
+      var pri = false;      
+      var pub = false;            
+      var nat = false;      
+      var numeroProvincias = 22;                  
+      var modulo = 11;
+                                 
+                      
+      if (numero.length < 10 ){              
+		console.log('la cedula:' + cedula + ' es incorrecta');
+		valido.className = "fa fa-times fa-2x";
+		valido.style.color="red";
+		valido.style.visibility="visible";
+      }
+     
+      /* Los primeros dos digitos corresponden al codigo de la provincia */
+      provincia = numero.substr(0,2);      
+      if (provincia < 1 || provincia > numeroProvincias){          
+
+		console.log('la cedula:' + cedula + ' es incorrecta');
+		valido.className = "fa fa-times fa-2x";
+		valido.style.color="red";
+		valido.style.visibility="visible";
+      
+      }
+
+      /* Aqui almacenamos los digitos de la cedula en variables. */
+      d1  = numero.substr(0,1);         
+      d2  = numero.substr(1,1);         
+      d3  = numero.substr(2,1);         
+      d4  = numero.substr(3,1);         
+      d5  = numero.substr(4,1);         
+      d6  = numero.substr(5,1);         
+      d7  = numero.substr(6,1);         
+      d8  = numero.substr(7,1);         
+      d9  = numero.substr(8,1);         
+      d10 = numero.substr(9,1);                
+         
+      /* El tercer digito es: */                           
+      /* 9 para sociedades privadas y extranjeros   */         
+      /* 6 para sociedades publicas */         
+      /* menor que 6 (0,1,2,3,4,5) para personas naturales */ 
+
+      if (d3==7 || d3==8){           
+		console.log('la cedula:' + cedula + ' es incorrecta');
+		valido.className = "fa fa-times fa-2x";
+		valido.style.color="red";
+		valido.style.visibility="visible";
+      }         
+         
+      /* Solo para personas naturales (modulo 10) */         
+      if (d3 < 6){           
+         nat = true;            
+         p1 = d1 * 2;  if (p1 >= 10) p1 -= 9;
+         p2 = d2 * 1;  if (p2 >= 10) p2 -= 9;
+         p3 = d3 * 2;  if (p3 >= 10) p3 -= 9;
+         p4 = d4 * 1;  if (p4 >= 10) p4 -= 9;
+         p5 = d5 * 2;  if (p5 >= 10) p5 -= 9;
+         p6 = d6 * 1;  if (p6 >= 10) p6 -= 9; 
+         p7 = d7 * 2;  if (p7 >= 10) p7 -= 9;
+         p8 = d8 * 1;  if (p8 >= 10) p8 -= 9;
+         p9 = d9 * 2;  if (p9 >= 10) p9 -= 9;             
+         modulo = 10;
+      }         
+         
+
+      if(nat == true){         
+         if (digitoVerificador != d10){                          
+            console.log('la cedula:' + cedula + ' es incorrecta');
+			valido.className = "fa fa-times fa-2x";
+			valido.style.color="red";
+			valido.style.visibility="visible";
+         }         
+        
+      }      
+	  console.log('la cedula:' + cedula + ' es correcta');
+	  valido.className = "fa fa-check fa-2x";
+	  valido.style.color="#69ca27";
+	  valido.style.visibility="visible";   
+   
+  }
+
+
