b154ddba573e8ac0cc797bd8305891c422632737
Diego Oswaldo Loor Moran <diego18.loor@gmail.com>
2021-04-08 12:01:50 -0500

    Avance 2

--- Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-consultas/medicina-general-consultas.component.ts
+++ Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-consultas/medicina-general-consultas.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MedicinaGeneralConsultasComponent implements OnInit {
 
   constructor() { }
-
+  isCollapsed = false;
   ngOnInit(): void {
   }
 
