a91b62e22a5257cf17aa408f99f201ac8169481e
ADZF28 <adzf2804@gmail.com>
2021-04-08 13:04:28 -0500

    Nuevo Estilo

--- Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-datos-pacientes/medicina-general-datos-pacientes.component.ts
+++ Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-datos-pacientes/medicina-general-datos-pacientes.component.ts
@@ -16,6 +16,10 @@ export class MedicinaGeneralDatosPacientesComponent implements OnInit {
   isCollapsed3 = true;
   Ver: boolean = true;
   Ocultar: boolean = false;
+  Ver2: boolean = true;
+  Ocultar2: boolean = false;
+  Ver3: boolean = true;
+  Ocultar3: boolean = false;
 
   public sidebarMinimized = false;
   public navItems = navItems;
@@ -26,16 +30,30 @@ export class MedicinaGeneralDatosPacientesComponent implements OnInit {
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
-
-  funcionVer(){
-    if(this.Ver == true){
+    funcionVer(){
       this.Ocultar = false;
       this.Ver = false;
-    }
   }
   funcionOcultar(){
       this.Ver = true;
       this.Ocultar = true;
+  }
+
+  funcionVer2(){
+      this.Ocultar2 = false;
+      this.Ver2 = false;
+  }
+  funcionOcultar2(){
+      this.Ver2 = true;
+      this.Ocultar2 = true;
+  }
+    funcionVer3(){
+      this.Ocultar3 = false;
+      this.Ver3 = false;
+  }
+  funcionOcultar3(){
+      this.Ver3 = true;
+      this.Ocultar3 = true;
   }
 
 }
