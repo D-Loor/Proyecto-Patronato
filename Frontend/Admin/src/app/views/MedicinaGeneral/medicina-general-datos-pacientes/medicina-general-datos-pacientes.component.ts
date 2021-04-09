9baade84c708a34dc6e3673a9eb634d181c6f214
ADZF28 <adzf2804@gmail.com>
2021-04-09 18:42:33 -0500

    dosModulos

--- Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-datos-pacientes/medicina-general-datos-pacientes.component.ts
+++ Frontend/Admin/src/app/views/MedicinaGeneral/medicina-general-datos-pacientes/medicina-general-datos-pacientes.component.ts
@@ -8,21 +8,37 @@ import { navItems } from '../../../_nav';
 })
 export class MedicinaGeneralDatosPacientesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {  }
 
   
   isCollapsed = false;
   isCollapsed2 = false;
-  isCollapsed3 = true;
+  isCollapsed3 = false;
+  isCollapsed4 = false;
+  isCollapsed5 = false;
+  isCollapsed6 = false;
+  isCollapsed7 = false;
   Ver: boolean = true;
   Ocultar: boolean = false;
   Ver2: boolean = true;
   Ocultar2: boolean = false;
   Ver3: boolean = true;
   Ocultar3: boolean = false;
+  EstadoVida: boolean = true;
+  nombres:string;
+  estado:string;
+  estadoT:string;
+  union:string;
+  union2:string;
+  number: number = 0;
+  DatosFamiliares: any = [];
 
   public sidebarMinimized = false;
   public navItems = navItems;
+
+  
+  
+
 
   ngOnInit(): void {
   }
@@ -30,31 +46,61 @@ export class MedicinaGeneralDatosPacientesComponent implements OnInit {
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
-    funcionVer(){
-      this.Ocultar = false;
-      this.Ver = false;
+  funcionVer(){
+    this.Ocultar = false;
+    this.Ver = false;
   }
   funcionOcultar(){
-      this.Ver = true;
-      this.Ocultar = true;
+    this.Ver = true;
+    this.Ocultar = true;
   }
 
   funcionVer2(){
-      this.Ocultar2 = false;
-      this.Ver2 = false;
+    this.Ocultar2 = false;
+    this.Ver2 = false;
   }
   funcionOcultar2(){
-      this.Ver2 = true;
-      this.Ocultar2 = true;
+    this.Ver2 = true;
+    this.Ocultar2 = true;
   }
-    funcionVer3(){
-      this.Ocultar3 = false;
-      this.Ver3 = false;
+  funcionVer3(){
+    this.Ocultar3 = false;
+    this.Ver3 = false;
   }
   funcionOcultar3(){
-      this.Ver3 = true;
-      this.Ocultar3 = true;
+    this.Ver3 = true;
+    this.Ocultar3 = true;
   }
+  vidaVer(){
+    this.EstadoVida = true;
+  }
+  vidaOcultar(){
+    this.EstadoVida = false;
+  }
+
+  LlenarArray(){
+    this.number ++;
+    if(this.union == "Otro"){
+      this.union = this.union2;
+    }
+    if(this.estadoT == null || this.estadoT== ""){
+      this.estadoT = "Sin causas";
+    }
+    let DatosFamiliares2 = [
+      {
+          "nombresF": this.nombres,
+          "unionF": this.union,
+          "estadoF": this.estado,
+          "causasF":this.estadoT,
+      }];
+      this.DatosFamiliares.push(DatosFamiliares2);
+  }
+
+  EliminarDatosArray(elimina:string){
+    this.DatosFamiliares.splice(this.DatosFamiliares.indexOf(dato => dato.nombresF === elimina), 1);
+    this.number --;
+  }
+  
   
 
 }
