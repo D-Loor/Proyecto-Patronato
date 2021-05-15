83fad973fd524a2f3862590130d4b87e70cffb2b
ADZF28 <adzf2804@gmail.com>
2021-05-15 15:52:54 -0500

    Titulo

--- Backend/resources/views/ConsolidadoMensualTerapia.blade.php
+++ Backend/resources/views/ConsolidadoMensualTerapia.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Morbilidad Medcina Genenral</title>
+    <title>Consolidado Mensual Terapia</title>
     <style>
         @page {
             margin: 0cm 1cm;
