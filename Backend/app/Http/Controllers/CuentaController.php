9c88d6792f5de938c5ad58500fcd03b47da9b6ea
ADZF28 <adzf2804@gmail.com>
2021-06-02 23:07:00 -0500

    update

--- Backend/app/Http/Controllers/CuentaController.php
+++ Backend/app/Http/Controllers/CuentaController.php
@@ -96,18 +96,19 @@ class CuentaController extends Controller
     {
         $datos=Cuenta::where('id_cuenta', $id)->get()->first();
 
-        $file=$request->file('Aqui va el ID');
-        $nombre=$file->getClientMimeType();
-        $tipoImagen=str_replace('image/', '.',$nombre);
-        $fileName=uniqid() . $tipoImagen;
-        $path=public_path().'/imagenes';
-        $file->move($path,$fileName); 
+        $file = $request->file('imagen');
+        $filename = $file->getClientOriginalName();
+        $filename = pathinfo($filename, PATHINFO_FILENAME);
+        $name_File = str_replace(" ", "_", $filename);
+        $extension = $file->getClientOriginalExtension();
+        $picture = date('His').'-'.$name_File.'.'.$extension;
+        $file->move(public_path('/imagenes'),$picture); 
 
         $datos->id_rol=$request->id_rol;
         $datos->nombres=$request->nombres;
         $datos->correo=$request->correo;
         $datos->password=$request->password;
-        $datos->imagen=$fileName;
+        $datos->imagen=$picture;
         $datos->update();
         
         return response()->json(['mensaje'=>"Dato Actualizado.", 'code'=>'201']);
