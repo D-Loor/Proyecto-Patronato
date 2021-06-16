f3f9b492fe8208266151fe5c2f0c9c902111ae56
ADZF28 <adzf2804@gmail.com>
2021-06-16 15:05:27 -0500

    Add migracion

---
+++ Backend/database/migrations/2021_03_27_001429_create_antecedente__patologico__familiars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAntecedentePatologicoFamiliarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('antecedente_patologico_familiares', function (Blueprint $table) {
+            $table->bigIncrements('id_a_p_familiar');
+            $table->unsignedBigInteger('id_familiar');
+            $table->unsignedBigInteger('id_paciente');
+
+            $table->foreign('id_paciente')->references('id_paciente')->on('pacientes')->onDelete('cascade');
+            $table->foreign('id_familiar')->references('id_familiar')->on('familiares')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('antecedente_patologico_familiares');
+    }
+}
